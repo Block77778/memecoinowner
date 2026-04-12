f7530ddb064039d1647d420eb79e7fa36e4e4641
Block77778 <theblockchainsolutions@gmail.com>
2026-04-13 03:14:49 +0500

    Update index.tsx

--- src/views/feature/index.tsx
+++ src/views/feature/index.tsx
@@ -1,149 +1,108 @@
 import { FC } from "react";
 import pkg from "../../../package.json";
+import { LuArrowRightFromLine } from "react-icons/lu";
+import { MdGeneratingTokens, MdToken } from "react-icons/md";
+import { RiTokenSwapFill } from "react-icons/ri";
+import { RxTokens } from "react-icons/rx";
 
-export const OfferView: FC = () => {
+type FeatureViewProps = {
+  setOpenCreateModal: (v: boolean) => void;
+  setOpenTokenMetaData: (v: boolean) => void;
+  setOpenContact: (v: boolean) => void;
+  setOpenAirdrop: (v: boolean) => void;
+  setOpenSendTransaction: (v: boolean) => void;
+};
+export const FeatureView: FC<FeatureViewProps> = ({
+  setOpenTokenMetaData,
+  setOpenContact,
+  setOpenAirdrop,
+  setOpenSendTransaction,
+  setOpenCreateModal,
+}) => {
+  const feature = [
+    {
+      name: "Token Generator",
+      icon: <MdGeneratingTokens />,
+      description:
+        "Start working with Solana Token Creator, It allows you to create solana token by Creating, deploying, airdrop, transfering and updating metadata.",
+      function: setOpenCreateModal,
+    },
+    {
+      name: "Get Airdrop",
+      icon: <MdToken />,
+      description:
+        "Start working with Solana Token Creator, It allows you to create solana token by Creating, deploying, airdrop, transfering and updating metadata.",
+      function: setOpenAirdrop,
+    },
+    {
+      name: "Transfer Sol",
+      icon: <RiTokenSwapFill />,
+      description:
+        "Start working with Solana Token Creator, It allows you to create solana token by Creating, deploying, airdrop, transfering and updating metadata.",
+      function: setOpenSendTransaction,
+    },
+    {
+      name: "Metadata Token",
+      icon: <RxTokens />,
+      description:
+        "Start working with Solana Token Creator, It allows you to create solana token by Creating, deploying, airdrop, transfering and updating metadata.",
+      function: setOpenTokenMetaData,
+    },
+  ];
   return (
-    <section id="features" className="py-20">
+    <section className="py-20">
       <div className="container">
         <div className="mb-10 flex items-end justify-between">
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="mb-4 text-3xl font-medium capitalize text-white">
-              Solana token popularity
+              Choose Solana Blockchain generator
             </h2>
             <p className="text-default-200 text-sm font-medium">
-              tart working with Solana Token Creator, It allows you to create
-              solana token by Creating, deploying, airdrop, transfering and
-              updating metadata.
+              Now you can create Solana Token <br />
+              to without code instantly.
             </p>
           </div>
         </div>
 
-        <div className="grid gap-6 lg:grid-cols-3">
-          <div className="space-y-6">
-            <div className="bg-default-950/40 hover:-trandefault-y-2 border-primary border-s-2 rounded-xl backdrop-blur-3xl transition-all duration-500">
-              <div className="p-10">
-                <i data-lucide="file-text" className="text-primary h-10 w-10"></i>
-                <h3 className="mb-4 mt-8 text-2xl font-medium text-white">
-                  Best Token Builder
-                </h3>
-                <p className="text-default-100 mb-4 text-sm font-medium">
-                  AI can create informative articles or blog posts on a wide
-                  range of topics.
+        <div className="bg-default-950/40 flex flex-wrap items-center rounded-3xl backdrop-blur-3xl">
+          {feature.map((list, index) => (
+            <div
+              key={list.name}
+              className={` w-auto grow border-b border-white/10  md:w-1/2
+              ${
+                index == 0
+                  ? "md:border-e   "
+                  : index == 1
+                  ? " "
+                  : index == 2
+                  ? "md:border-e md:border-b-0"
+                  : " "
+              } `}
+            >
+              <div className="p-8 sm:p-10">
+                <div className="bg-primary/10 text-primary mb-10 inline-flex h-16 w-16 items-center justify-center rounded-xl">
+                  <i data-lucide="framer" className="">
+                    {list.icon}
+                  </i>
+                </div>
+                <h2 className="mb-4 text-2xl font-medium text-white">
+                  {list.name}
+                </h2>
+                <p className="text-default-200 mb-6 text-base">
+                  {list.description}
                 </p>
                 <a
-                  href="#"
-                  className="text-primary group relative inline-flex items-center gap-2"
+                  onClick={() => list.function(true)}
+                  className="hover:bg-primary-hover inline-flex items-center justify-center gap-2 rounded-full border border-white/10 px-6 py-2 text-white transition-all duration-300"
                 >
-                  <span className="bg-primary/80 absolute -bottom-0 h-px w-7/12 rounded transition-all duration-500 group-hover:w-full"></span>
-                  Read More <i data-lucide="move-right" className="h-4 w-4"></i>
+                  More Tools{" "}
+                  <i data-lucide="move-right" className="">
+                    <LuArrowRightFromLine />
+                  </i>
                 </a>
               </div>
             </div>
-            <div className="bg-default-950/40 hover:-trandefault-y-2 border-primary border-s-2 rounded-xl backdrop-blur-3xl transition-all duration-500">
-              <div className="p-10">
-                <i data-lucide="pen" className="text-primary h-10 w-10"></i>
-                <h3 className="mb-4 mt-8 text-2xl font-medium text-white">
-                  Updated Metadata
-                </h3>
-                <p className="text-default-100 mb-4 text-sm font-medium">
-                  Ai can generate short stories, poetry, or other creative
-                  pieces.
-                </p>
-                <a
-                  href="#"
-                  className="text-primary group relative inline-flex items-center gap-2"
-                >
-                  <span className="bg-primary/80 absolute -bottom-0 h-px w-7/12 rounded transition-all duration-500 group-hover:w-full"></span>
-                  Read More <i data-lucide="move-right" className="h-4 w-4"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="space-y-6">
-            <div className="bg-default-950/40 hover:-trandefault-y-2 border-primary border-s-2 rounded-xl backdrop-blur-3xl transition-all duration-500">
-              <div className="p-10">
-                <i data-lucide="database" className="text-primary h-10 w-10"></i>
-                <h3 className="mb-4 mt-8 text-2xl font-medium text-white">
-                  Data Analysis and Solana
-                </h3>
-                <p className="text-default-100 mb-4 text-sm font-medium">
-                  AI can analyze data and generate reports with insights and
-                  Financial Reports.
-                </p>
-                <a
-                  href="#"
-                  className="text-primary group relative inline-flex items-center gap-2"
-                >
-                  <span className="bg-primary/80 absolute -bottom-0 h-px w-7/12 rounded transition-all duration-500 group-hover:w-full"></span>
-                  Read More <i data-lucide="move-right" className="h-4 w-4"></i>
-                </a>
-              </div>
-            </div>
-            <div className="bg-default-950/40 hover:-trandefault-y-2 border-primary border-s-2 rounded-xl backdrop-blur-3xl transition-all duration-500">
-              <div className="p-10">
-                <i data-lucide="gitlab" className="text-primary h-10 w-10"></i>
-                <h3 className="mb-4 mt-8 text-2xl font-medium text-white">
-                  Code and Programming Solana
-                </h3>
-                <p className="text-default-100 mb-4 text-sm font-medium">
-                  Code Snippets: AI can generate code snippets in various
-                  programming languages.
-                </p>
-                <a
-                  href="#"
-                  className="text-primary group relative inline-flex items-center gap-2"
-                >
-                  <span className="bg-primary/80 absolute -bottom-0 h-px w-7/12 rounded transition-all duration-500 group-hover:w-full"></span>
-                  Read More <i data-lucide="move-right" className="h-4 w-4"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="space-y-6">
-            <div className="bg-default-950/40 hover:-trandefault-y-2 border-primary border-s-2 rounded-xl backdrop-blur-3xl transition-all duration-500">
-              <div className="p-10">
-                <i data-lucide="palette" className="text-primary h-10 w-10"></i>
-                <h3 className="mb-4 mt-8 text-2xl font-medium text-white">
-                  Zero Code Solana
-                </h3>
-                <p className="text-default-100 mb-4 text-sm font-medium">
-                  Graphic Design: AI can generate images, logos, and other
-                  visual content
-                </p>
-                <a
-                  href="#"
-                  className="text-primary group relative inline-flex items-center gap-2"
-                >
-                  <span className="bg-primary/80 absolute -bottom-0 h-px w-7/12 rounded transition-all duration-500 group-hover:w-full"></span>
-                  Read More <i data-lucide="move-right" className="h-4 w-4"></i>
-                </a>
-              </div>
-            </div>
-            <div className="bg-default-950/40 hover:-trandefault-y-2 border-primary border-s-2 rounded-xl backdrop-blur-3xl transition-all duration-500">
-              <div className="p-10">
-                <i
-                  data-lucide="case-sensitive"
-                  className="text-primary h-10 w-10"
-                ></i>
-                <h3 className="mb-4 mt-8 text-2xl font-medium text-white">
-                  Solana Tools
-                </h3>
-                <p className="text-default-100 mb-4 text-sm font-medium">
-                  AI can trandefault text from one language to another and Ai
-                  can any language your choice.
-                </p>
-                <a
-                  href="#"
-                  className="text-primary group relative inline-flex items-center gap-2"
-                >
-                  <span className="bg-primary/80 absolute -bottom-0 h-px w-7/12 rounded transition-all duration-500 group-hover:w-full"></span>
-                  Read More <i data-lucide="move-right" className="h-4 w-4"></i>
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
